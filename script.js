babeaeb92bbfaff785b4bc4dbea4a56b5b81d74f
Noel-auki <151185389+Noel-auki@users.noreply.github.com>
2024-09-10 18:03:38 +0530

    reduced cross fade 1 sec

--- script.js
+++ script.js
@@ -49,7 +49,7 @@
     }
 
     function crossfade(fromGainNode, toGainNode) {
-        const duration = 2;  // Set the crossfade duration (in seconds)
+        const duration = 1;  // Set the crossfade duration (in seconds)
         fromGainNode.gain.exponentialRampToValueAtTime(0.01, audioCtx.currentTime + duration); // Fade out
         toGainNode.gain.setValueAtTime(0.01, audioCtx.currentTime);  // Start with a low volume
         toGainNode.gain.exponentialRampToValueAtTime(1, audioCtx.currentTime + duration);  // Fade in
